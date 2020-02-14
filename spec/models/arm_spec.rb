dbb38d237556abce189e7f02cb7c543bc66b49ab
TimeSmash <mcumm64@gmail.com>
2020-02-14 01:39:13 -0500

    Installed shoulda for easier test write-up

--- spec/models/arm_spec.rb
+++ spec/models/arm_spec.rb
@@ -1,8 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Arm, type: :model do
-  it "belongs to a Person" do
-    arm_owners = Arm.reflect_on_all_associations(:belongs_to)
-    expect(arm_owners.any?{|owner| owner.inspect.include?("@name=:person")}).to eq(true)
-  end
+  # it "belongs to a Person" do
+  #   arm_owners = Arm.reflect_on_all_associations(:belongs_to)
+  #   expect(arm_owners.any?{|owner| owner.inspect.include?("@name=:person")}).to eq(true)
+  # end
+
+  # Using Shoulda matchers
+  it { should belong_to(:person) }
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -37,6 +37,13 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    # Choose a test framework:
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
 # Configure the Shoulda Matchers so Rspec is used as test framework and Rails as the library for matchers
 
 RSpec.configure do |config|
@@ -46,6 +53,8 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+
+  
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
